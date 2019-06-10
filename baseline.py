73e31255c5b1fdf8e396abce5d2aa1f999c1f7a0
Benjamin Petit <benpetit@stanford.edu>
2019-06-09 19:54:03 -0700

    Added tensorboard logging

--- baseline.py
+++ baseline.py
@@ -11,15 +11,28 @@ env = gym.make('MsPacman-v0')
 env = DummyVecEnv([lambda: env])
 model = PPO2(MlpPolicy, env, verbose=0, tensorboard_log="./logs/baseline_MDP")
 
+eval_scores = []
+for ep in range(10):
+    obs = env.reset()
+    total_reward = 0
+    for i in range(1000):
+        action, _states = model.predict(obs)
+        obs, reward, done, info = env.step(action)
+        total_reward += reward
+    eval_scores.append(total_reward)
+print(f"Before learning: {np.mean(eval_scores)}")
+
 for k in range(300):
     print(f"Epoch {k}")
     model.learn(total_timesteps=1000)
     if k%10 == 0:
-        for ep in range(3):
+        eval_scores = []
+        for ep in range(10):
             obs = env.reset()
             total_reward = 0
             for i in range(1000):
                 action, _states = model.predict(obs)
                 obs, reward, done, info = env.step(action)
                 total_reward += reward
-            print(f"Episode {ep}: {total_reward}")
+            eval_scores.append(total_reward)
+        print(f"Mean score: {np.mean(eval_scores)}")
